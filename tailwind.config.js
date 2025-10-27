1ae89be0badb2c13f34d605c00fd74717a5e0308
Daredevil-Left <renzosamanamud@gmail.com>
2025-10-26 21:24:40 -0500

    Migración completa a Tailwind local

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}", "./src/**/*.{html,js}"], // ajusta si tus archivos están en otra carpeta
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
